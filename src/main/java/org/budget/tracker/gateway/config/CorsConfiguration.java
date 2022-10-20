a16ca8c201705cb95715dd574850d481ade4408e
Amol Singh <amol1.singh@scotiabank.com>
2022-10-20 09:51:11 -0400

    cors

--- src/main/java/org/budget/tracker/gateway/config/CorsConfiguration.java
+++ src/main/java/org/budget/tracker/gateway/config/CorsConfiguration.java
@@ -10,21 +10,21 @@ import java.util.Arrays;
 @Configuration
 public class CorsConfiguration extends org.springframework.web.cors.CorsConfiguration {
 
-    @Bean
-    public CorsWebFilter corsFilter() {
-        org.springframework.web.cors.CorsConfiguration corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
-        corsConfiguration.setAllowCredentials(true);
-        //corsConfiguration.addAllowedOrigin("http://localhost:3000");
-        corsConfiguration.addAllowedOrigin("https://budget-tracker-4de96.web.app");
-        // corsConfiguration.addAllowedOrigin("https://budget-tracker-4de96.firebaseapp.com");
-        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
-        corsConfiguration.addAllowedHeader("origin");
-        corsConfiguration.addAllowedHeader("content-type");
-        corsConfiguration.addAllowedHeader("accept");
-        corsConfiguration.addAllowedHeader("authorization");
-        corsConfiguration.addAllowedHeader("cookie");
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfiguration);
-        return new CorsWebFilter(source);
-    }
+//    @Bean
+//    public CorsWebFilter corsFilter() {
+//        org.springframework.web.cors.CorsConfiguration corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+//        corsConfiguration.setAllowCredentials(true);
+//        //corsConfiguration.addAllowedOrigin("http://localhost:3000");
+//        // corsConfiguration.addAllowedOrigin("https://budget-tracker-4de96.web.app");
+//        // corsConfiguration.addAllowedOrigin("https://budget-tracker-4de96.firebaseapp.com");
+//        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+//        corsConfiguration.addAllowedHeader("origin");
+//        corsConfiguration.addAllowedHeader("content-type");
+//        corsConfiguration.addAllowedHeader("accept");
+//        corsConfiguration.addAllowedHeader("authorization");
+//        corsConfiguration.addAllowedHeader("cookie");
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", corsConfiguration);
+//        return new CorsWebFilter(source);
+//    }
 }
