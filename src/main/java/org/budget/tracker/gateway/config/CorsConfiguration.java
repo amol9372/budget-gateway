49890c41713db89d8ab2f354d07742f764c5b3f0
Amol Singh <amol1.singh@scotiabank.com>
2022-10-20 08:35:58 -0400

    cors

---
+++ src/main/java/org/budget/tracker/gateway/config/CorsConfiguration.java
@@ -0,0 +1,30 @@
+package org.budget.tracker.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfiguration extends org.springframework.web.cors.CorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        org.springframework.web.cors.CorsConfiguration corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("http://localhost:3000");
+        corsConfiguration.addAllowedOrigin("https://budget-tracker-4de96.web.app");
+        corsConfiguration.addAllowedOrigin("https://budget-tracker-4de96.firebaseapp.com");
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        corsConfiguration.addAllowedHeader("origin");
+        corsConfiguration.addAllowedHeader("content-type");
+        corsConfiguration.addAllowedHeader("accept");
+        corsConfiguration.addAllowedHeader("authorization");
+        corsConfiguration.addAllowedHeader("cookie");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
